5e71679928867710d3e329a68d6d1f8d818d1442
Shivayan Chakravarty <shivayanchak@gmail.com>
2026-02-11 00:48:30 +0530

    fe crash issue

--- client_next/app/page.tsx
+++ client_next/app/page.tsx
@@ -1,18 +1,19 @@
-import { useAppContext } from "@/context/AppContext";
 import { cookies } from "next/headers";
 import { redirect } from "next/navigation";
 
 export default async function RootPage() {
   const cookieStore = await cookies();
   const token = cookieStore.get("token")?.value;
-  const {isLoggedIn, userData} = useAppContext();
-  if (!isLoggedIn) {
+  if (!token) {
     redirect("/auth/login");
   }
-  if (!userData) {
+  const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL!;
+  const res = await fetch(BACKEND_URL + "/api/auth/me", { headers: { Cookie: `token=${token}`, }, cache: "no-store", });
+  if (!res.ok) {
     redirect("/auth/login");
   }
-  if (!userData.isProfileComplete) {
+  const { user } = await res.json();
+  if (!user.isProfileComplete) {
     redirect("/auth/profile");
   }
   redirect("/main");
